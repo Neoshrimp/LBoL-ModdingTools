8d54293d45c8f6a5d44604cc1bee9051044f1ecb
Neoshrimp <shadow638@protonmail.com>
2023-08-28 14:33:22 +0300

    Add cwt test

---
+++ src/CWT_test/Plugin.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Runtime.CompilerServices;
+using BepInEx;
+using HarmonyLib;
+using Mono.Cecil;
+using UnityEngine;
+
+
+namespace CWT_test
+{
+    [BepInPlugin(GUID, "CWT test", version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public const string GUID = "neo.tests.cwtTest";
+        public const string version = "1.0.0";
+
+        private static readonly Harmony harmony = new Harmony(GUID);
+
+        internal static BepInEx.Logging.ManualLogSource log;
+
+        private void Awake()
+        {
+            log = Logger;
+
+            // very important. Without this the entry point MonoBehaviour gets destroyed
+            DontDestroyOnLoad(gameObject);
+            gameObject.hideFlags = HideFlags.HideAndDontSave;
+
+            harmony.PatchAll();
+
+            StartCoroutine(ConditionalWeakTableTest.Test());
+
+        }
+
+
+
+        private void OnDestroy()
+        {
+            if (harmony != null)
+                harmony.UnpatchSelf();
+        }
+
+
+        
+
+
+
+    }
+
+
+
+
+ 
+public class ConditionalWeakTableTest
+    {
+        private static void Collect()
+        {
+            System.GC.Collect();
+            System.GC.WaitForPendingFinalizers();
+        }
+
+        static object[] refArray = new object[100];
+
+        static ConditionalWeakTable<object, object> cwt = new ConditionalWeakTable<object, object>();
+
+
+        public static IEnumerator Test()
+        {
+            object a = new object();
+            object b = new object();
+
+/*            for (var i = 0; i < refArray.Length; i++)
+            {
+                refArray[i] = new object();
+                cwt.Add(refArray[i], new object());
+
+            }*/
+
+            cwt.Add(a, b);
+
+            var wa = new System.WeakReference(a);
+            var wb = new System.WeakReference(b);
+
+            Plugin.log.LogInfo(wa.IsAlive);
+            Plugin.log.LogInfo(wb.IsAlive);
+
+            yield return null;
+            Collect();
+
+            Plugin.log.LogInfo(wa.IsAlive);
+            Plugin.log.LogInfo(wb.IsAlive);
+
+            b = null;
+
+            for (int i = 0; i < 10; ++i)
+            {
+                yield return null;
+                Collect();
+
+                Plugin.log.LogInfo(wa.IsAlive);
+                Plugin.log.LogInfo(wb.IsAlive);
+            }
+
+            a = null;
+
+            for (int i = 0; i < 1000; ++i)
+            {
+                yield return null;
+                Collect();
+            }
+
+            Plugin.log.LogInfo($"If CWS worked, this would be false: {wa.IsAlive}");
+            Plugin.log.LogInfo($"If CWS worked, this would be false: {wb.IsAlive}");
+            Plugin.log.LogInfo($"cwt size: {AccessTools.Field(typeof(ConditionalWeakTable<object,object>), "size").GetValue(cwt)}");
+
+        }
+    }
+
+}
---
+++ src/CWT_test/README.md
@@ -0,0 +1,3 @@
+the post: https://forum.unity.com/threads/does-conditionalweaktable-work.617893/
+
+tested on Mono tags 2018.4.32 and 2021.3.27. In 2021 cwt worked while in 2018 it did not.
