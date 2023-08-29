5a9d8605f8b73f631d2168afd127e9be40bf44a3
Neoshrimp <shadow638@protonmail.com>
2023-08-29 19:19:18 +0300

    Add NoSteam

---
+++ src/NoSteam/Plugin.cs
@@ -0,0 +1,104 @@
+﻿using BepInEx;
+using HarmonyLib;
+using LBoL.Core;
+using LBoL.Core.PlatformHandlers;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+namespace NoSteam
+{
+    [BepInPlugin(GUID, "No Steam", version)]
+    [BepInProcess("LBoL.exe")]
+    public class Plugin : BaseUnityPlugin
+    {
+        public const string GUID = "neo.lbol.debug.NoSteam";
+        public const string version = "1.0.0";
+
+        private static readonly Harmony harmony = new Harmony(GUID);
+
+        internal static BepInEx.Logging.ManualLogSource log;
+
+        private void Awake()
+        {
+            log = Logger;
+
+            // very important. Without this the entry point MonoBehaviour gets destroyed
+            DontDestroyOnLoad(gameObject);
+            gameObject.hideFlags = HideFlags.HideAndDontSave;
+
+            harmony.PatchAll();
+
+        }
+
+        private void OnDestroy()
+        {
+            if (harmony != null)
+                harmony.UnpatchSelf();
+        }
+
+
+        [HarmonyPatch(typeof(SteamPlatformHandler), nameof(SteamPlatformHandler.Init))]
+        class SteamPlatformHandlerInit_Patch
+        {
+            static void Prefix()
+            {
+
+            }
+            static void Postfix(ref bool __result, SteamPlatformHandler __instance)
+            {
+                __result = true;
+                var steamId = Directory.GetDirectories(Application.persistentDataPath).Select(
+                    d => Regex.Match(d, @"\d+$", RegexOptions.RightToLeft)).First().Value;
+
+                if (!string.IsNullOrEmpty(steamId))
+                    __instance._steamId = ulong.Parse(steamId);
+                
+            }
+        }
+
+
+        [HarmonyPatch]
+        class SteamPlatformHandler_Patch
+        {
+
+
+            static IEnumerable<MethodBase> TargetMethods()
+            {
+                yield return AccessTools.Method(typeof(SteamPlatformHandler), nameof(SteamPlatformHandler.Update));
+                yield return AccessTools.Method(typeof(SteamPlatformHandler), nameof(SteamPlatformHandler.Shutdown));
+                yield return AccessTools.Method(typeof(SteamPlatformHandler), nameof(SteamPlatformHandler.SetMainMenuInfo));
+                yield return AccessTools.Method(typeof(SteamPlatformHandler), nameof(SteamPlatformHandler.SetGameRunInfo));
+
+
+            }
+
+            static bool Prefix()
+            {
+                return false;
+            }
+
+        }
+
+
+        [HarmonyPatch(typeof(SteamPlatformHandler), nameof(SteamPlatformHandler.GetDefaultLocale))]
+        class SteamPlatformHandlerGetDefaultLocale_Patch
+        {
+            static bool Prefix(ref Locale __result)
+            {
+                __result = Locale.En;
+                return false;
+            }
+
+        }
+
+
+
+
+
+
+    }
+}
---
+++ src/NoSteam/README.md
@@ -0,0 +1,4 @@
+## Run the game without Steam
+
+put the plugin dll to plugins folder, put `steam_appid.txt` to LBoL.exe folder
+Doesn't work if the game is being ran for the very first time after a fresh install.
