9b44e067b3e6e8fe7b07804e70fe3544fe795eb7
Neoshrimp <shadow638@protonmail.com>
2024-06-19 03:18:55 +0300

    Add logging header

--- src/Logging/CsvLogger.cs
+++ src/Logging/CsvLogger.cs
@@ -4,6 +4,9 @@ using System.Text;
 using System.IO;
 using UnityEngine;
 using System.Linq;
+using System.Runtime.Serialization;
+using HarmonyLib;
+using LBoL.Base.Extensions;
 
 namespace Logging
 {
@@ -16,19 +19,36 @@ namespace Logging
 
         private static readonly string SessionFolder = $"_{DateTime.Now:yyyy-MM-dd_HH.mm.ss}";
 
-        
+        private static ObjectIDGenerator objectAssociations = new ObjectIDGenerator();
+        private static int assCount = 0;
 
         private readonly FileStream _fileStream;
         private readonly StreamWriter _streamWriter;
         private readonly object lockObj = new object();
+        private bool isEnabled = true;
 
         public readonly string logFile;
         public readonly string subFolder;
         public readonly string fileDir;
-        private List<string> values = new List<string>();
 
-        public List<string> Values { get => values; set => values = value; }
+        private List<string> header = new List<string>();
+        private Dictionary<string, string> values = new Dictionary<string, string>();
+        private string emptyValue = "N/A";
 
+        public static int AssCount { get => assCount; }
+        public IReadOnlyList<string> Header { get => header; }
+        public IReadOnlyDictionary<string, string> Values { get => values; }
+        public string EmptyValue 
+        {
+            get => emptyValue; 
+            set 
+            { 
+                values.Keys.Where(k => values[k] == emptyValue).Do(k => values[k] = value); 
+                emptyValue = value; 
+            } 
+        }
+
+        public bool IsEnabled { get => isEnabled; set => isEnabled = value; }
 
         private CsvLogger(string logFile, string ext = ".csv", string subFolder = "")
         {
@@ -42,39 +62,92 @@ namespace Logging
             this.subFolder = subFolder;
         }
 
-        public static CsvLogger GetOrCreateLog(string logFile, string ext = ".csv", string subFolder = "")
+        public static CsvLogger GetOrCreateLog(string logFile, object ass, string ext = ".csv", string subFolder = "", bool isEnabled = true)
         {
-            if (!loggers.TryGetValue(logFile, out CsvLogger logger))
+            
+            var assId = objectAssociations.GetId(ass, out var firstTime);
+            if (firstTime) assCount++;
+            var fileId = logFile + assId.ToString();;
+            if (!loggers.TryGetValue(fileId, out CsvLogger logger))
             {
-                logger = new CsvLogger(logFile, ext);
-                loggers[logFile] = logger;
+                logger = new CsvLogger(logFile + "_" + assCount.ToString(), ext, subFolder);
+                loggers[fileId] = logger;
+                logger.isEnabled = isEnabled;
             }
             return logger;
         }
 
-        public static CsvLogger L(string logFile, string ext = ".csv") => GetOrCreateLog(logFile, ext);
 
+        public static CsvLogger L(string logFile, string ext = ".csv") => GetOrCreateLog(logFile, ext);
 
         public static bool TryGetLogger(string logFile, out CsvLogger logger) => loggers.TryGetValue(logFile, out logger);
 
-        public static int Count() => loggers.Count;
 
-        public void AddVal(object val) => values.Add(val.ToString());
+        public void AddHeaderVal(string valKey)
+        {
+            if (valKey == null || values.ContainsKey(valKey))
+                return;
+            header.Add(valKey);
+            values.Add(valKey, emptyValue);
+        }
 
-        public void AddVals(object[] vals) => values.AddRange(vals.Select(v => v.ToString()));
+        public void SetHeader(IEnumerable<string> collumns)
+        {
+            header.Clear();
+            values.Clear();
+            collumns.Do(c => AddHeaderVal(c));
+        }
 
-        public bool HasVals() => values.Count > 0;
+        public void LogHead() => Log(header);
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="val"></param>
+        /// <param name="valKey"></param>
+        /// <param name="doThrow"></param>
+        /// <returns>False if valKey is not in the header</returns>
+        /// <exception cref="ArgumentException"></exception>
+        public bool SetVal(object val, string valKey, bool doThrow = true)
+        {
+            if (!isEnabled)
+                return false;
+            if (values.ContainsKey(valKey))
+            {
+                values[valKey] = val?.ToString() ?? "null";
+                return true;
+            }
+
+            if (doThrow)
+                throw new ArgumentException($"CsvLogger {logFile} doesn't have column {valKey}");
+
+            return false;
+        }
+
+        public bool SetVals(IEnumerable<KeyValuePair<string, string>> valsNKeys) => valsNKeys.Select(kv => SetVal(kv.Value, kv.Key)).Any();
+
+        public bool HasVals() => values.Values.Any(v => v != emptyValue);
 
         public void FlushVals()
         {
-            Log(values.ToArray());
-            values.Clear();
+            var toLog = new string[header.Count];
+            var i = 0;
+            foreach (var k in header)
+            {
+                toLog[i] = values[k];
+                values[k] = emptyValue;
+                i++;
+            }
+            Log(toLog);
         }
 
-        public void Log(object[] values) => Log(String.Join(", ", values));
+        public void Log(IEnumerable<object> values) => Log(String.Join(", ", values));
 
         public void Log(string message)
         {
+            if (!IsEnabled)
+                return;
             var logEntry = message;
 
             lock (lockObj)
